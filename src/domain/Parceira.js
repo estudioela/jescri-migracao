d612fd7a40c36c1d7873df02c50d08e023108c7a
Daniel Perrut <elafashionmkt@gmail.com>
2026-07-15 00:43:29 -0300

    feat(domain): adiciona entidade Parceira

---
+++ src/domain/Parceira.js
@@ -0,0 +1,19 @@
+/**
+ * ENTIDADE: Parceira
+ *
+ * Primeira entidade de domínio do TEAR V2.
+ *
+ * Representa uma parceira cadastrada no programa de colaboração.
+ *
+ * Responsabilidades:
+ * - preservar invariantes do domínio;
+ * - controlar estado Ativa/Inativa;
+ * - não conhecer infraestrutura.
+ *
+ * Não pode conhecer:
+ * - SpreadsheetApp
+ * - HTML
+ * - HTTP
+ * - Repository
+ * - ACL
+ */
